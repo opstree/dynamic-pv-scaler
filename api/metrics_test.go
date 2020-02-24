92cd22d997ba7aa52d782fdd8b39ef52b2109579
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-02-24 14:30:22 +0530

    Added test cases

--- api/metrics_test.go
+++ api/metrics_test.go
@@ -1,31 +1,6 @@
 package api
 
 import (
-	"reflect"
-	"testing"
-
-	"k8s.io/client-go/kubernetes"
-)
-
-func TestCreateClient(t *testing.T) {
-	tests := []struct {
-		name string
-		want *kubernetes.Clientset
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CreateClient(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CreateClient() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-root@527300ccc2d0:/go/src/dynamic-pv-scaler# cat api/metrics_test.go 
-package api
-
-import (
 	"net/http"
 	"reflect"
 	"testing"
