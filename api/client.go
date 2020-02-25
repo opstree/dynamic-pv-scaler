6a3d4b947576991001857186796ce6a10f11b80c
Kartik Chopra <kartik.chopra@opstree.com>
2020-02-25 17:22:34 +0530

    formatting ..

--- api/client.go
+++ api/client.go
@@ -11,7 +11,7 @@ const (
 	kubeConfig = "InClusterConfig"
 )
 
-// CreateClient function returns are kubernetes client */
+// CreateClient function returns are kubernetes client 
 func CreateClient() *kubernetes.Clientset {
 	config, err := rest.InClusterConfig()
 	logger.LogStdout()
