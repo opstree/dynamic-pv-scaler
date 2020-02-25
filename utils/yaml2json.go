b0590737d203d0f57b70378aae96ba53d9cbac20
Kartik Chopra <kartik.chopra@opstree.com>
2020-02-25 13:18:06 +0530

    comments added for yaml2json.go

--- utils/yaml2json.go
+++ utils/yaml2json.go
@@ -7,8 +7,13 @@ import (
 	"io/ioutil"
 	"os"
 )
-
+	/* 
+	Converts given YAML file to json
+	Takes filename as input and returns results as interface.
+	*/
 func yamlToJson(filename string) []map[string]interface{} {
+	
+
 	file, err := ioutil.ReadFile(filename)
 	checkError(err)
 
@@ -20,14 +25,20 @@ func yamlToJson(filename string) []map[string]interface{} {
 	return results
 }
 
+	// Checks for any error
 func checkError(err error) bool {
+	
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	return (err != nil)
 }
 
+	// Gets configFile location , converts it to json and returns the same.
 func GetConfigurations() []map[string]interface{} {
+ 
+
 	configFile := os.Getenv("CONFIG_FILE")
 	config := yamlToJson(configFile)
 	return config
