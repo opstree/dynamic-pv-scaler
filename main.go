9562f71f9ecaf3a78c9bf76507905248241aad88
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2022-09-04 22:28:50 +0530

    Added structure for CRD
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- main.go
+++ main.go
@@ -1,53 +1,115 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
-	"dynamic-pv-scaling/api"
-	"dynamic-pv-scaling/logger"
-	"dynamic-pv-scaling/pkg"
-	"dynamic-pv-scaling/utils"
-	"fmt"
-	"github.com/jasonlvhit/gocron"
-	log "github.com/sirupsen/logrus"
-	"strconv"
-	"time"
+	"flag"
+	"os"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	pvcv1 "dynamic-pv-scaler/api/v1"
+	"dynamic-pv-scaler/controllers"
+	//+kubebuilder:scaffold:imports
 )
 
 var (
-	nameSpace           string
-	scalePercentage     int
-	thresholdPercentage int
-	pvcName             string
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
 )
 
-// ResizeFunction : Is the main driver code checks for PV usage and scales up the PV when and as defined.
-func ResizeFunction() {
-	infos := utils.GetConfigurations()
-	logger.LogStdout()
-	for _, info := range infos {
-		nameSpace = fmt.Sprintf("%v", info["namespace"])
-		scalePercentage, _ = strconv.Atoi(fmt.Sprintf("%v", info["scale_percentage"]))
-		thresholdPercentage, _ = strconv.Atoi(fmt.Sprintf("%v", info["threshold_percentage"]))
-		pvcName = fmt.Sprintf("%v", info["pvc_name"])
-		if (100 - api.GetPersistentVolumeList(nameSpace, pvcName).Value) >= thresholdPercentage {
-			updatedSize := utils.CalculateUpdatedSize(api.GetPeristentVolumeUsage(nameSpace, pvcName).Value, scalePercentage)
-			pkg.ResizePersistentVolume(pvcName, nameSpace, updatedSize)
-			for _, pod := range pkg.ListPods(nameSpace) {
-				if pod.PersistentVolumeName == pvcName {
-					time.Sleep(100 * time.Second)
-					pkg.DeletePod(pod.PodName, nameSpace)
-				}
-			}
-		} else {
-			log.WithFields(log.Fields{
-				"namespace":              nameSpace,
-				"persistent_volume_name": pvcName,
-			}).Info("Threshold is under control, no need of resizing")
-		}
-	}
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(pvcv1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
 }
 
 func main() {
-	scheduler := gocron.NewScheduler()
-	scheduler.Every(100).Seconds().Do(ResizeFunction)
-	<-scheduler.Start()
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "e1b2856a.scaler.opstreelabs.in",
+		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
+		// when the Manager ends. This requires the binary to immediately end when the
+		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
+		// speeds up voluntary leader transitions as the new leader don't have to wait
+		// LeaseDuration time first.
+		//
+		// In the default scaffold provided, the program ends immediately after
+		// the manager stops, so would be fine to enable this option. However,
+		// if you are doing or is intended to do any operation such as perform cleanups
+		// after the manager stops then its usage might be unsafe.
+		// LeaderElectionReleaseOnCancel: true,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.PersistentVolumeScalerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PersistentVolumeScaler")
+		os.Exit(1)
+	}
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
